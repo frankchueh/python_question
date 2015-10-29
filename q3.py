f0f32f3382458501fa21ec3e15ae7421cf29bcea
frankchueh <as8307044@gmail.com>
2015-10-29 10:29:43 +0800

    question3 motify

--- q3.py
+++ q3.py
@@ -1,10 +1,10 @@
 with open("file3.txt") as f:
 
-	data = f.readline().strip('"').split('","')
-	sorted_data = sorted(data)
+	names = [ line.strip('"') for line in f.readline().split(',') ]
+	sorted_names = sorted(names)
 	name_score, index = 0, 0
 
-	for item in sorted_data:
+	for item in sorted_names:
 		index += 1
 		for letter in item:
 			name_score += (ord(letter)-64)*index
